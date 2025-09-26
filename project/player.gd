436d506572ba8eef0c799aa33e8d46ab1be79473
acc668 <116805246+acc668@users.noreply.github.com>
2025-09-25 22:06:05 -0700

    Update player.gd

--- project/player.gd
+++ project/player.gd
@@ -1,14 +1,14 @@
 extends CharacterBody2D
 
-signal player_action(action_name)
+signal playerAction(actionName)
 
 func _input(event):
     if event is InputEventKey and event.pressed:
         if event.keycode == KEY_SPACE:
-            player_action.emit("start_orbit")
+            playerAction.emit("startOrbit")
 
         elif event.keycode == KEY_P:
-            player_action.emit("start_pulse")
+            playerAction.emit("startPulse")
 
         elif event.keycode == KEY_S:
-            player_action.emit("stop_animations")
+            playerAction.emit("stopAnimations")
