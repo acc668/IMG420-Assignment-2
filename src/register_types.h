1d3ea768d23b63366f6a3266e185a6a61f9e7554
acc668 <116805246+acc668@users.noreply.github.com>
2025-09-25 22:12:42 -0700

    Update register_types.h

--- src/register_types.h
+++ src/register_types.h
@@ -2,7 +2,7 @@
 #define REGISTER_TYPES_H
 
 // declare necessary functions
-void initialize_enhanced_sprite_module();
-void uninitialize_enhanced_sprite_module();
+void initializeEnhancedSpriteModule();
+void uninitializeEnhancedSpriteModule();
 
 #endif
